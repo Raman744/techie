16f19ba5e048f2721aca5374a289e7ad87ab2291
Raman shah <ramanshah744@gmail.com>
2021-11-27 09:31:14 +0530

    27

--- ArrayEasy/ArrayPairUsingSorting.cpp
+++ ArrayEasy/ArrayPairUsingSorting.cpp
@@ -7,19 +7,26 @@ void findPair(int nums[], int n, int target)
     int high = n - 1;
     while (low < high)
     {
+        if (nums[low] + nums[high] > target)
+        {
+            high--;
+        }
+        if (nums[low] + nums[high] < target)
+        {
+            low--;
+        }
         if (nums[low] + nums[high] == target)
         {
-            cout << "pair found"
-                 << "(" << nums[low] << "," << nums[high] << ")" << endl;
-            return;
+            cout << "Pair found "
+                 << "(" << nums[low]
+                 << "," << nums[high] << ")";
+            low-- or high++;
         }
-        (nums[low] + nums[high] < target) ? low++ : high--;
     }
-    cout << "pair not found";
 }
 int main()
 {
-    int nums[] = {8, 7, 2, 5, 3, 1};
+    int nums[] = {8, 7, 2, 5, 3, 2};
     int target = 10;
     int n = sizeof(nums) / sizeof(nums[0]);
     findPair(nums, n, target);
